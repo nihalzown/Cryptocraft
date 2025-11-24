628ad4f82b5b0fd96ab36589136d5d40e6ef293a
MUHAMMED NIHAL <145740029+nihalzown@users.noreply.github.com>
2025-11-24 10:58:54 +0530

    handled requests request model created

--- backend/src/main/java/com/cryptocraft/backend/BackendApplication.java
+++ backend/src/main/java/com/cryptocraft/backend/BackendApplication.java
@@ -1,43 +1,12 @@
 package com.cryptocraft.backend;
 
-import java.beans.BeanProperty;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner testMyCode(EncryptionService service) {
-		return args -> {
-			try {
-                System.out.println("\n\n-------------------------------------");
-
-                // 1. Caesar Test
-                System.out.println("1. CAESAR (Shift 1): " + service.encryptCaesar("HAL", 1)); 
-
-                // 2. Substitution Test
-                String subKey = "QWERTYUIOPASDFGHJKLZXCVBNM";
-                System.out.println("2. SUBST (Hello):    " + service.encryptSubstitution("Hello", subKey));
-                
-                // 3. Playfair Test
-                System.out.println("3. PLAYFAIR:         " + service.playfairEncrypt("BALLOON", "MONARCHY"));
-                
-                // 4. DES Test
-                System.out.println("4. DES (Base64):     " + service.encryptDES("Secrets", "MyPassword"));
-
-                System.out.println("-------------------------------------\n\n");
-            } catch (Exception e) {
-                System.err.println("Error during encryption tests: " + e.getMessage());
-                e.printStackTrace();
-            }
-		};
 	}
 }
---
+++ backend/src/main/java/com/cryptocraft/backend/CryptoController.java
@@ -0,0 +1,61 @@
+package com.cryptocraft.backend;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
+public class CryptoController {
+    private final EncryptionService service;
+
+    public CryptoController(EncryptionService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/encrypt")
+    public Map<String, String> encryptData(@RequestBody EncryptionRequest request){
+        String algo = request.getAlgorithm();
+        String text = request.getPlaintext();
+        String key = request.getKey();
+        String result = "";
+
+        if(alogo== null || text == null || key == null ){
+            return Collections.singletonMap("error", "Missing algorithm,plaintext or key");
+        }
+
+        try{
+            switch(algo.toLowerCase()){
+                case "ceasar":
+                    try{
+                        int shift = Integer.parseInt(key);
+                        result = service.encryptCeasar(text,shift);
+                    } catch(NumberFormatException e){
+                        return Collections.singletonMap("error", "Caesar key must be a number");
+                    }break;
+                
+                    case "substitution":
+                        result = service.encryptSubstitution(text, key);
+                        break;
+                    
+                    case "playfair":
+                        result = service.playfairEncrypt(text, key);
+                        break;
+                    
+                    case "des":
+                        result = service.encryptDES(text, key);
+                        break;
+
+                    default:
+                        return Collections.singletonMap("error", "unknown algorithm: "+algo);
+            }
+        } catch (Exception e){
+            return Collections.singletonMap("error", "Encryption Failed: " + e.getMessage());
+        }
+
+
+        return Collections.singletonMap("result", result);
+    }
+}
+
