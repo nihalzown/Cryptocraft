f88f3ea43f7159c1c8828d6e487e84f61f4269ec
MUHAMMED NIHAL <145740029+nihalzown@users.noreply.github.com>
2025-11-24 09:18:24 +0530

    ceasar mono

---
+++ backend/src/main/java/com/cryptocraft/backend/encryptionservice.java
@@ -0,0 +1,15 @@
+package com.cryptocraft.backend;
+
+import org.springframework.stereotype.Service;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+@Service
+
+
+public class encryptionservice {
+    
+}
